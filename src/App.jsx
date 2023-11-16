43f627101525b7e3e246b11ad2c51cebe86111da
Marishkumark <marishkumark29@gmail.com>
2023-11-16 13:48:04 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import Navbar from "./component/Navbar";
+import CartContainer from "./component/CartContainer";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { calculateTotal } from "./features/cart/cartSlice";
+import Modal from "./component/Modal";
+
+function App() {
+  const { cartItems } = useSelector((store) => store.cart);
+  const { isOpen } = useSelector((store) => store.modal);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(calculateTotal());
+  }, [cartItems]);
+
+  return (
+    <main>
+      {isOpen && <Modal />}
+      <Navbar />
+      <CartContainer />
+    </main>
+  );
+}
+export default App;
